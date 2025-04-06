a4cf1261df1e2e641d4bb29c5aa29cc63409c0a9
Lucas Barbosa <lucas2304barbosa@gmail.com>
2025-04-06 16:16:33 -0300

    quase pronto

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
 
-const localStorage = "to-do-list"
+let input = document.querySelector('.input-tarefa'); 
+let lista = document.querySelector('.lista');
 
-function adicionarTarefa()
+let listaTarefas = [] // cria um array para guardar as tarefas
+
+console.log('MINHA LISTA', listaTarefas);
+rendenderizarListaDeTarefas()
+
+function adicionar()
 {
-    let input = document.querySelector(".input-task");
+    // ve se o campo ta vazio ou com espaço
+    if (input.value.trim() === "")
+    {
+        // dai se tiver ele mostra um aletra com essa mensagem
+        alert("Você precisa digitar algo antes de adicionar!");
+        return;
+    }
 
-    if(!input.value)
-    {
-        alert("Digite algo para escrever na sua lista");
-    }
-    else
-    {
-        let values = JSON.parse(localStorage.getItem(localStorage) || "[]")
-        values.push({
-            name: input.value
-        })
-        localStorage.setItem(localStorage,JSON.stringify(values))
-    }
+    listaTarefas.push(input.value.trim());
+    
+    rendenderizarListaDeTarefas()
 }
 
-function mostraValues()
+function rendenderizarListaDeTarefas()
 {
-    let values = JSON.parse(localStorage.getItem(localStorage) || "[]")
-    let lista = document.querySelector(".list-task")
-    lista.innerHTML = ''
+    // mostra no console como esta a lista no momento, se atualizar é criado outro
+    console.log('[RENDER] MINHA LISTA COMO FICOU', listaTarefas);
+
+    // aqui criei uma variavel pro HTML que vai mostrar na tela
+    let novoItem = ''
+
+    listaTarefas.forEach(item => {
+        console.log('um dos meus itens é ', item)
+
+        novoItem += `
+        <li class="task">
+             <p>${item}</p>
+            <button class="button-cancelar">x</button>
+        </li>
+        `;
+
+    })
+
+    lista.innerHTML = novoItem;
+    document.querySelector('.input-tarefa').value = '';
 }
+
 
